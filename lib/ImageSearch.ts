3361731282f50abbb7bbc3882f37e88dd30a2d44
Fritz Hoste <fritz@instantonline.be>
2019-03-11 10:54:32 +0100

    fix build

--- lib/ImageSearch.ts
+++ lib/ImageSearch.ts
@@ -14,37 +14,39 @@ export class ImageSearch {
         const $ = cheerio.load(response.html);
 
         const logos: string[] = [
-            {type: 'og:logo', url: $('meta[property="og:logo"]').attr('content')},
-            {type: 'meta-itemprop/logo', url: $('meta[itemprop="logo"]').attr('content')},
+            { type: 'og:logo', url: $('meta[property="og:logo"]').attr('content') },
+            { type: 'meta-itemprop/logo', url: $('meta[itemprop="logo"]').attr('content') },
             ...$('link[rel*="icon"]')
                 .map((i, el) => {
-                    return {type: 'link-rel/icon', url: $(el).attr('href'), size: $(el).attr('sizes')};
+                    return { type: 'link-rel/icon', url: $(el).attr('href'), size: $(el).attr('sizes') };
                 })
                 .get(),
-            {type: 'img-itemprop/logo', url: $('img[itemprop="logo"]').attr('src')},
+            { type: 'img-itemprop/logo', url: $('img[itemprop="logo"]').attr('src') },
             {
                 type: 'meta-name/msapplication-TileImage',
-                url: $('meta[name*="msapplication-TileImage"]').attr('content')
+                url: $('meta[name*="msapplication-TileImage"]').attr('content'),
             },
-            {type: 'meta-content/logo', url: $('meta[content*="logo"]').attr('content')},
-            {type: 'meta-content/image', url: $('meta[itemprop*="image"]').attr('content')},
+            { type: 'meta-content/logo', url: $('meta[content*="logo"]').attr('content') },
+            { type: 'meta-content/image', url: $('meta[itemprop*="image"]').attr('content') },
             ...$('script[type*="application/ld+json"]')
                 .map((i, el) => {
-                    return {type: 'json-ld-logo', url: Helpers.findJsonLdImages($(el).html())};
+                    return { type: 'json-ld-logo', url: Helpers.findJsonLdImages($(el).html()) };
                 })
                 .get(),
-            {type: 'img-alt/logo', url: $('img[alt*="logo"]').attr('src')},
-            {type: 'img-alt/logo-class', url: $('img[class*="logo"]').attr('src')},
-            {type: 'img-src/logo', url: $('img[src*="logo"]').attr('src')},
-            {type: 'og:image', url: $('meta[property="og:image"]').attr('content')},
-            {type: 'svg:image', data: true, url: Helpers.svgToDataURL($('a[class*="logo"]').html())},
+            { type: 'img-alt/logo', url: $('img[alt*="logo"]').attr('src') },
+            { type: 'img-alt/logo-class', url: $('img[class*="logo"]').attr('src') },
+            { type: 'img-src/logo', url: $('img[src*="logo"]').attr('src') },
+            { type: 'og:image', url: $('meta[property="og:image"]').attr('content') },
+            { type: 'svg:image', data: true, url: Helpers.svgToDataURL($('a[class*="logo"]').html()) },
         ].filter(e => e.url);
 
         const correctLogos: ImageData[] = logos.map((image: any) => {
-            return !Helpers.validUrl(image.url) && image.url.indexOf('data:') === -1 ? {
-                ...image,
-                url: response.url + image.url
-            } : image;
+            return !Helpers.validUrl(image.url) && image.url.indexOf('data:') === -1
+                ? {
+                      ...image,
+                      url: response.url + image.url,
+                  }
+                : image;
         });
 
         if (showAllImages) {
