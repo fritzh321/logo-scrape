59ed7a836e80bf1ddcce77d39aca73f23d4faa4f
rad1k4l <zeynalli169@gmail.com>
2021-04-17 20:24:07 +0400

    code cleanup and fix

--- lib/Helpers.ts
+++ lib/Helpers.ts
@@ -1,25 +1,28 @@
 export class Helpers {
-    public static validUrl(url: string): boolean {
-        const isValidUrl: RegExp = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
-        return isValidUrl.test(url);
+  public static validUrl(url: string): boolean {
+    const isValidUrl: RegExp = /^(?:http(s)?:\/\/)?[\w.-]+(?:\.[\w\.-]+)+[\w\-\._~:/?#[\]@!\$&'\(\)\*\+,;=.]+$/;
+    return isValidUrl.test(url);
+  }
+
+  public static findJsonLdImages(text: string | null): any {
+    if (text == null) {
+      return null;
+    }
+    const info = JSON.parse(text);
+    return info ? info.logo : null;
+  }
+
+  public static svgToDataURL(svgStr: string | null): any {
+    if (svgStr && svgStr.indexOf('svg') && svgStr.indexOf('href') === -1) {
+      const encoded = encodeURIComponent(svgStr)
+        .replace(/'/g, '%27')
+        .replace(/"/g, '%22');
+
+      const header = 'data:image/svg+xml,';
+      return header + encoded;
+    } else {
+      return null;
     }
 
-    public static findJsonLdImages(text: string): any {
-        const info = JSON.parse(text);
-        return info ? info.logo : null;
-    }
-
-    public static svgToDataURL(svgStr: string) {
-        if (svgStr && svgStr.indexOf('svg') && svgStr.indexOf('href') === -1) {
-            const encoded = encodeURIComponent(svgStr)
-                .replace(/'/g, '%27')
-                .replace(/"/g, '%22');
-
-            const header = 'data:image/svg+xml,';
-            return header + encoded;
-        } else {
-            return null;
-        }
-
-    }
+  }
 }
